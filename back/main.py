05668a95430e96058d662069966a49b2570209b1
Rita Haimov <shlomihaimov1@gmail.com>
2023-11-27 19:52:09 +0200

    webhookcheck5

--- back/main.py
+++ back/main.py
@@ -7,7 +7,7 @@ CORS(app, support_credentials=True)
 
 @app.route('/', methods=['POST', 'GET'])
 def main():
-    return {'res': True, 'data': '5'}
+    return {'res': True, 'data': '6'}
 
 # main driver function
 if __name__ == '__main__':
